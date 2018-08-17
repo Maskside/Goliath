587e6af78129792f0567a954df95c514cee6e59e
AllanVester <vesterallan246@gmail.com>
2018-08-18 01:26:31 +0200

    Added Allans Rod - Part 1

---
+++ Allans Rod/Visual Studio 2013/Projects/Loading/loading/Form1.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Drawing.Drawing2D
+Public Class Form1
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.CenterToParent()
+        Me.SetStyle(ControlStyles.ResizeRedraw, True)
+        Me.SetStyle(ControlStyles.DoubleBuffer, True)
+        Me.SetStyle(ControlStyles.AllPaintingInWmPaint, True)
+    End Sub
+    Private Sub Form1_Paint(ByVal sender As Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Me.Paint
+        Static i As Integer
+        i += 1
+        If i = 360 Then
+            i = 0
+        End If
+        Me.BackgroundImage = DrawProgressBar(e.Graphics, i)
+    End Sub
+    Private Function DrawProgressBar(ByVal g As Graphics, ByVal angle As Integer) As Bitmap
+        Dim bmp As New Bitmap(Me.ClientRectangle.Width, Me.ClientRectangle.Height)
+        g = Graphics.FromImage(bmp)
+        Dim percent As Double
+        percent = Math.Round(angle * 100 / 360, 1)
+        Dim path As New GraphicsPath
+        Dim rec As Rectangle = New Rectangle(0, 0, bmp.Width, bmp.Height)
+        Dim stringformat As New StringFormat
+        stringformat.Alignment = StringAlignment.Center
+        stringformat.LineAlignment = StringAlignment.Center
+        g.DrawString(percent & " %", New Font("Segoe UI", 32, FontStyle.Bold), Brushes.Green, New Point(rec.Width / 2, rec.Height / 2), stringformat)
+        g.DrawEllipse(Pens.Black, rec)
+        path.AddPie(rec, 180, angle)
+        Dim holeRect As Rectangle = New Rectangle(rec.X + 15, rec.Y + 15, rec.Width - 30, rec.Height - 30)
+        g.DrawEllipse(Pens.Black, holeRect)
+        g.Clip = (New Region(path))
+        path.AddPie(holeRect, 180, angle)
+        Dim gradiant As New LinearGradientBrush(rec, Color.Red, Color.Blue, 45)
+        g.FillPath(gradiant, path)
+        Invalidate(rec)
+        Return bmp
+    End Function
+End Class
---
+++ Allans Rod/Visual Studio 2013/Projects/Month Week and Date/month week and date/Form1.Designer.vb
@@ -0,0 +1,68 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.WeekOfMonthEdit1 = New DevExpress.XtraScheduler.UI.WeekOfMonthEdit()
+        Me.WeekOfMonthEdit2 = New DevExpress.XtraScheduler.UI.WeekOfMonthEdit()
+        CType(Me.WeekOfMonthEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.WeekOfMonthEdit2.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'WeekOfMonthEdit1
+        '
+        Me.WeekOfMonthEdit1.Location = New System.Drawing.Point(301, 177)
+        Me.WeekOfMonthEdit1.Name = "WeekOfMonthEdit1"
+        Me.WeekOfMonthEdit1.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.WeekOfMonthEdit1.Size = New System.Drawing.Size(100, 20)
+        Me.WeekOfMonthEdit1.TabIndex = 0
+        '
+        'WeekOfMonthEdit2
+        '
+        Me.WeekOfMonthEdit2.Location = New System.Drawing.Point(338, 231)
+        Me.WeekOfMonthEdit2.Name = "WeekOfMonthEdit2"
+        Me.WeekOfMonthEdit2.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.WeekOfMonthEdit2.Size = New System.Drawing.Size(100, 20)
+        Me.WeekOfMonthEdit2.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(661, 430)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.WeekOfMonthEdit2)
+        Me.Controls.Add(Me.WeekOfMonthEdit1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Text = "Form1"
+        CType(Me.WeekOfMonthEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.WeekOfMonthEdit2.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents WeekOfMonthEdit1 As DevExpress.XtraScheduler.UI.WeekOfMonthEdit
+    Friend WithEvents WeekOfMonthEdit2 As DevExpress.XtraScheduler.UI.WeekOfMonthEdit
+
+End Class
